dc5decdc6b6c1fcf2e37ef8227d69905c0adcb3b
Aaron Lane <10655063+aaron-lane@users.noreply.github.com>
2019-12-11 14:20:27 -0500

    Fix versioning argument syntax

--- modules/cloudbuild/main.tf
+++ modules/cloudbuild/main.tf
@@ -80,7 +80,7 @@ resource "google_storage_bucket" "cloudbuild_artifacts" {
   location           = var.default_region
   labels             = var.storage_bucket_labels
   bucket_policy_only = true
-  versioning = {
+  versioning {
     enabled = true
   }
 }
