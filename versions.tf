3dd21ace3462bc36195b5b673f0e9b8d575cf3c8
Morgante Pell <morgantep@google.com>
2020-01-22 15:18:06 -0500

    Relax Google provider version constraints

--- versions.tf
+++ versions.tf
@@ -18,7 +18,7 @@ terraform {
   required_version = "~> 0.12.6"
 
   required_providers {
-    google      = "~> 2.1"
-    google-beta = "~> 2.1"
+    google      = ">= 2.1, <4.0"
+    google-beta = ">= 2.1, <4.0"
   }
 }
