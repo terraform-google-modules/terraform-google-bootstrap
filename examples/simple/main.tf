43e0f4162e3eb8299e85ae37eba17b8ae54b5514
Rohan Jerrems <rjerrems@google.com>
2020-07-22 11:55:39 +1000

    fix: bump to latest provider version

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -15,11 +15,11 @@
  */
 
 provider "google" {
-  version = "~> 3.9.0"
+  version = "~> 3.31.0"
 }
 
 provider "google-beta" {
-  version = "~> 3.9.0"
+  version = "~> 3.31.0"
 }
 
 provider "null" {
