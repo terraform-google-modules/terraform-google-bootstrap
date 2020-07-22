0266871c19911a7266fa7f2b98f5e561dc749850
Rohan Jerrems <rjerrems@google.com>
2020-07-22 10:45:59 +1000

    feat: Add support for terraform validator

--- modules/cloudbuild/main.tf
+++ modules/cloudbuild/main.tf
@@ -241,7 +241,7 @@ resource "null_resource" "cloudbuild_terraform_builder" {
       gcloud builds submit ${path.module}/cloudbuild_builder/ \
       --project ${module.cloudbuild_project.project_id} \
       --config=${path.module}/cloudbuild_builder/cloudbuild.yaml \
-      --substitutions=_TERRAFORM_VERSION=${var.terraform_version},_TERRAFORM_VERSION_SHA256SUM=${var.terraform_version_sha256sum}
+      --substitutions=_TERRAFORM_VERSION=${var.terraform_version},_TERRAFORM_VERSION_SHA256SUM=${var.terraform_version_sha256sum},_TERRAFORM_VALIDATOR_RELEASE=${var.terraform_validator_release}
   EOT
   }
   depends_on = [
