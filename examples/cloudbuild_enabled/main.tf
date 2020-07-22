771b8261895ef2d0abe63b488aba2a41e4d61a15
Rohan Jerrems <rjerrems@google.com>
2020-07-22 11:24:39 +1000

    fix: bump provider and project factory version

--- examples/cloudbuild_enabled/main.tf
+++ examples/cloudbuild_enabled/main.tf
@@ -16,11 +16,11 @@
 
 
 provider "google" {
-  version = "~> 3.5.0"
+  version = "~> 3.9.0"
 }
 
 provider "google-beta" {
-  version = "~> 3.5.0"
+  version = "~> 3.9.0"
 }
 
 provider "null" {
--- test/setup/main.tf
+++ test/setup/main.tf
@@ -16,7 +16,7 @@
 
 module "project" {
   source  = "terraform-google-modules/project-factory/google"
-  version = "~> 5.0"
+  version = "~> 8.0"
 
   name              = "ci-bootstrap"
   random_project_id = true
