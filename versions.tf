b9187635254e7fb7350cc5a513907ac1f6afa987
Ryan Fisher <ryan@techfish.io>
2020-10-15 11:49:48 -0700

    Upgrade module to support Terraform 0.13 [EN-4343]
    [EN-4343]: https://sightly.atlassian.net/browse/EN-4343

--- versions.tf
+++ versions.tf
@@ -15,10 +15,18 @@
  */
 
 terraform {
-  required_version = "~> 0.12.20"
-
   required_providers {
-    google      = "~> 3.9"
-    google-beta = "~> 3.9"
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.9"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.9"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
   }
+  required_version = ">= 0.13"
 }
