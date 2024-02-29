00bb18f99824562657ba387ed2e0f79989f910fb
Joseph Thomas <josephdthomas@google.com>
2024-02-29 18:04:26 +0000

    Fix typo referencing wrong resource

--- modules/im_cloudbuild_workspace/github.tf
+++ modules/im_cloudbuild_workspace/github.tf
@@ -69,7 +69,7 @@ data "google_secret_manager_secret_version" "existing_github_pat_secret_version"
 
 resource "google_secret_manager_secret_iam_policy" "existing_github_secret_iam_policy" {
   count       = local.create_github_secret ? 0 : 1
-  project     = google_secret_manager_secret.existing_github_token_secret[0].project
-  secret_id   = google_secret_manager_secret.existing_github_token_secret[0].secret_id
+  project     = google_secret_manager_secret.existing_github_pat_secret[0].project
+  secret_id   = google_secret_manager_secret.existing_github_pat_secret[0].secret_id
   policy_data = data.google_iam_policy.serviceagent_secretAccessor.policy_data
 }
