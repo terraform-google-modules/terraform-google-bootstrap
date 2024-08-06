aa3ed4124b0ca12fea1edfd663c5740624cf5672
Daniel Andrade <dandrade@ciandt.com>
2024-08-06 17:21:47 -0300

    make subnetworks distinct

--- examples/tf_cloudbuild_workspace_simple_gitlab/files/main.tf
+++ examples/tf_cloudbuild_workspace_simple_gitlab/files/main.tf
@@ -23,19 +23,19 @@ module "test-vpc-module" {
 
   subnets = [
     {
-      subnet_name   = "subnet-01"
+      subnet_name   = "subnet-gl-01"
       subnet_ip     = "10.10.10.0/24"
       subnet_region = "us-west1"
     },
     {
-      subnet_name           = "subnet-02"
+      subnet_name           = "subnet-gl-02"
       subnet_ip             = "10.10.20.0/24"
       subnet_region         = "us-west1"
       subnet_private_access = "true"
       subnet_flow_logs      = "true"
     },
     {
-      subnet_name               = "subnet-03"
+      subnet_name               = "subnet-gl-03"
       subnet_ip                 = "10.10.30.0/24"
       subnet_region             = "us-west1"
       subnet_flow_logs          = "true"
