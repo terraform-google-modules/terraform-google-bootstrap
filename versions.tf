d00764d560cd9c50c78895f796ce05c722fc7333
Quoc Trung HOANG <quoc-trung.hoang@webedia-group.com>
2025-11-19 09:11:11 +0100

    chore: bump google provider constraints to v7
    Signed-off-by: Quoc Trung HOANG <quoc-trung.hoang@webedia-group.com>

--- versions.tf
+++ versions.tf
@@ -20,7 +20,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.50, < 7"
+      version = ">= 3.50, < 8"
     }
     random = {
       source  = "hashicorp/random"
